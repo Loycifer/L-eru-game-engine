5dee4e9b51253ff8bb272ab36185ae3ac99c1652
Loy Clements <loy.clements@gmail.com>
2014-02-26 21:59:45 +0100

    Updated Layer.addObjects()
    Layer.addObjects() can now be overloaded with multiple objects

--- L-layer.js
+++ L-layer.js
@@ -2,24 +2,26 @@ var L;
 
 L.objects.Layer = function(targetContext)
 {
-this.sorted = false;
-this.sortBy = "x";
-this.objects = [];
-this.targetContext = targetContext;
-this.layerAlpha = 1;
-this.isClickable = true;
+    this.sorted = false;
+    this.sortBy = "z";
+    this.objects = [];
+    this.targetContext = targetContext;
+    this.layerAlpha = 1;
+    this.isClickable = true;
 };
 
 
 L.objects.Layer.prototype.draw = function()
 {
-  this.objects.draw(this.targetContext);
+    this.objects.draw(this.targetContext);
 };
 
 
 L.objects.Layer.prototype.update = function()
 {
-    this.objects.mapQuick(function(object){object.update();});
+    this.objects.mapQuick(function(object) {
+	object.update();
+    });
     if (this.sorted)
     {
 	var sortBy = this.sortBy;
@@ -28,16 +30,25 @@ L.objects.Layer.prototype.update = function()
 };
 
 
-L.objects.Layer.prototype.isClicked = function(mouseX,mouseY)
+L.objects.Layer.prototype.isClicked = function(mouseX, mouseY)
 {
 
     if (this.isClickable)
     {
-	this.objects.isClicked(mouseX,mouseY);
+	this.objects.isClicked(mouseX, mouseY);
     }
 };
 
 L.objects.Layer.prototype.addObject = function(object)
 {
     this.objects.push(object);
+};
+
+L.objects.Layer.prototype.addObjects = function(objects)
+{
+    var objectsLength = arguments.length;
+    for (var i = 0; i < objectsLength; i++)
+    {
+	this.addObject(arguments[i]);
+    }
 };
