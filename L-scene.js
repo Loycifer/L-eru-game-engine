e35470d50fe043c19f8d86ba1b8f8334c075201d
Loy Clements <loy.clements@gmail.com>
2014-05-28 11:57:12 +0200

    Scene Update
    Scenes initialize with one layer,
    .addObject method adds object to scene 0.

--- L-scene.js
+++ L-scene.js
@@ -2,7 +2,7 @@ var L;
 L.objects.Scene = function(name)
 {
     L.scenes[name] = this;
-    this.layers = [];
+    this.layers = [new L.objects.Layer(L.system.bufferContext[0])];
     this.bgFill = "blueviolet";
     this.motionBlur = 0.5;
     this.keymap = {};
@@ -21,9 +21,9 @@ L.objects.Scene.prototype.doKeyDown = function(event)
 
 L.objects.Scene.prototype.autoUpdate = function(dt)
 {
-    
+
     this.layers.update(dt);
-   
+
 };
 
 L.objects.Scene.prototype.draw = function()
@@ -33,11 +33,11 @@ L.objects.Scene.prototype.draw = function()
 
 L.objects.Scene.prototype.autoDraw = function()
 {
-    	L.system.bufferContext[0].fillStyle = this.bgFill;
-	L.system.bufferContext[0].fillRect(0, 0, L.system.width, L.system.height);
-	this.layers.draw();
-    	L.system.renderContext[0].globalAlpha = this.motionBlur;
-	L.system.renderContext[0].drawImage(L.system.bufferCanvas[0], 0, 0, L.system.width, L.system.height);
+    L.system.bufferContext[0].fillStyle = this.bgFill;
+    L.system.bufferContext[0].fillRect(0, 0, L.system.width, L.system.height);
+    this.layers.draw();
+    L.system.renderContext[0].globalAlpha = this.motionBlur;
+    L.system.renderContext[0].drawImage(L.system.bufferCanvas[0], 0, 0, L.system.width, L.system.height);
 };
 
 L.objects.Scene.prototype.addLayer = function(howMany)
@@ -49,6 +49,11 @@ L.objects.Scene.prototype.addLayer = function(howMany)
     }
 };
 
+L.objects.Scene.prototype.addObject = function(object)
+{
+    this.layers[0].addObject(object);
+};
+
 L.objects.Scene.prototype.isClicked = function(mouseX, mouseY)
 {
 
@@ -57,21 +62,21 @@ L.objects.Scene.prototype.isClicked = function(mouseX, mouseY)
 
 
 L.objects.Scene.prototype.transition = {};
-	
+
 L.objects.Scene.prototype.transition.fadeToColor = function(nextScene, fadeOut, pause, fadeIn, color, callback)
 {
-    
+
     L.transitions.fadeToColor.play(L.system.currentScene, nextScene, fadeOut, pause, fadeIn, color, callback);
 };
 
 L.objects.Scene.prototype.transition.fadeToBlack = function(nextScene, fadeOut, pause, fadeIn, callback)
 {
-    
+
     L.transitions.fadeToColor.play(L.system.currentScene, nextScene, fadeOut, pause, fadeIn, "#000000", callback);
 };
 
 L.objects.Scene.prototype.transition.instant = function(nextScene, callback)
 {
-    
+
     L.transitions.instant.play(L.system.currentScene, nextScene, callback);
 };
