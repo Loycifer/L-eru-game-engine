a525d65eaed67f6b4d79a664ac81b5b71a3bf0fc
Loy Clements <loy.clements@gmail.com>
2014-05-30 15:35:46 +0200

    Textbox Multiline Update
    Textboxes now support multiple lines and word-wrapping

--- L-textbox.js
+++ L-textbox.js
@@ -1,20 +1,39 @@
 var L;
-L.objects.Textbox = function(text, x, y, width, height)
+L.objects.Textbox = function(text, x, y, width, height, wordwrap)
 {
+
     //this.text = [];
-    this.text = text || "Textbox";
+    //this.text = text || "Textbox";
+    //this.width = width;
+    //this.height = height;
+    this.words = [];
+    this.textArray = [];
+    Object.defineProperty(this, "text", {
+	set: function(text)
+	{
+	    this.words = text.split(" ");
+	    // this.autoSizeX();
+	    this.wrapText();
+	}.bind(this),
+	get: function() {
+	    return this.words.join(" ");
+	}.bind(this)
+    });
+
+
     this.x = x;
     this.y = y;
     this.alpha = 1;
     this.font = "Times";
     this.fontSize = 30;
+    this.lineSpacing = 1;
 
     this.handle = {};
     this.handle.x = 0;
     this.handle.y = 0;
 
     this.angle = 0;
-    
+
     this.speedX = 0;
     this.speedY = 0;
     this.accelX = 0;
@@ -37,7 +56,7 @@ L.objects.Textbox = function(text, x, y, width, height)
     this.marginTop = 5;
     this.marginRight = 5;
     this.marginBottom = 5;
-    
+
     this.isClickable = true;
 };
 
@@ -54,17 +73,22 @@ L.objects.Textbox.prototype.autoDraw = function(layer)
     layer.fillStyle = this.color;
     layer.textAlign = "left";//this.alignment;
     layer.font = this.fontSize + "px " + this.font;
+    if (!this.height)
+    {
+	this.height = this.fontSize;
+    }
+    var arrayLength = this.textArray.length;
     if (this.angle !== 0)
     {
 	var radians = this.angle;
 	layer.save();
-	layer.translate(this.x, this.y );
+	layer.translate(this.x, this.y);
 	layer.rotate(-radians);
-	
-	
+
+
 	layer.beginPath();
 	layer.fillStyle = this.backgroundFill;
-	layer.rect( - this.handle.x, - this.handle.y, this.width + this.marginLeft + this.marginRight, this.height + this.marginTop + this.marginBottom);
+	layer.rect(-this.handle.x, -this.handle.y, this.width + this.marginLeft + this.marginRight, this.height + this.marginTop + this.marginBottom + (this.fontSize * (arrayLength - 1) * this.lineSpacing));
 	layer.fill();
 	if (this.borderWidth > 0)
 	{
@@ -74,17 +98,20 @@ L.objects.Textbox.prototype.autoDraw = function(layer)
 	}
 	layer.fillStyle = this.textFill;
 	layer.textBaseline = "bottom";
-	layer.fillText(this.text, this.marginLeft - this.handle.x, this.marginTop + this.fontSize - this.handle.y);
-	
-	
-	
+	for (var i = 0; i < arrayLength; i++)
+	{
+	    layer.fillText(this.textArray[i], this.marginLeft - this.handle.x, this.marginTop + this.fontSize - this.handle.y + (this.fontSize * i * this.lineSpacing));
+	}
+
+
+
 	//layer.fillText(this.text, 0, 0);
 	layer.restore();
     } else {
 
 	layer.beginPath();
 	layer.fillStyle = this.backgroundFill;
-	layer.rect(this.x - this.handle.x, this.y - this.handle.y, this.width + this.marginLeft + this.marginRight, this.height + this.marginTop + this.marginBottom);
+	layer.rect(this.x - this.handle.x, this.y - this.handle.y, this.width + this.marginLeft + this.marginRight, this.height + this.marginTop + this.marginBottom + (this.fontSize * (arrayLength - 1) * this.lineSpacing));
 	layer.fill();
 	if (this.borderWidth > 0)
 	{
@@ -94,7 +121,9 @@ L.objects.Textbox.prototype.autoDraw = function(layer)
 	}
 	layer.fillStyle = this.textFill;
 	layer.textBaseline = "bottom";
-	layer.fillText(this.text, this.x + this.marginLeft - this.handle.x, this.y + this.marginTop + this.fontSize - this.handle.y);
+
+	for (var i = 0; i < arrayLength; i++)
+	    layer.fillText(this.textArray[i], this.x + this.marginLeft - this.handle.x, this.y + this.marginTop + this.fontSize - this.handle.y + (this.fontSize * i * this.lineSpacing));
     }
 };
 
@@ -106,7 +135,7 @@ L.objects.Textbox.prototype.update = function(dt)
 
 L.objects.Textbox.prototype.autoUpdate = function(dt)
 {
-    
+
 };
 
 L.objects.Textbox.prototype.autoSize = function()
@@ -126,70 +155,103 @@ L.objects.Textbox.prototype.autoSizeY = function()
     this.height = this.fontSize;
 };
 
-L.objects.Textbox.prototype.getTextWidth = function()
+L.objects.Textbox.prototype.wrapText = function()
 {
-    L.system.bufferContext[0].font = this.fontSize + "px " + this.font;
-    var metrics = L.system.bufferContext[0].measureText(this.text);
-    return metrics.width;    
+    var arrayLength = this.words.length;
+    this.textArray = [];
+    var currentLineNumber = 0;
+    var currentLineText = "";
+    var textBoxWidth = this.width;
+    for (var i = 0; i < arrayLength; i++)
+    {
+	var currentLineWidth = this.getTextWidth(currentLineText);
+	if (currentLineWidth === 0)
+	{
+	    currentLineText = this.words[i];
+	}
+	else if (this.getTextWidth(currentLineText + " " + this.words[i]) <= textBoxWidth)
+	{
+	    currentLineText += " " + this.words[i];
+	}
+	else
+	{
+	    this.textArray[currentLineNumber] = currentLineText;
+	    currentLineNumber++;
+	    currentLineText = this.words[i];
+	    //  alert(this.words[i]);
+	}
+
+    }
+    this.textArray[currentLineNumber] = currentLineText;
 };
 
+L.objects.Textbox.prototype.getTextWidth = function(text)
+{
+    L.system.bufferContext[0].font = this.fontSize + "px " + this.font;
+    if (text === "")
+    {
+	return 0;
+    }
+    var metrics = L.system.bufferContext[0].measureText(text ? text : this.text);
+    return metrics.width;
+};
 
 L.objects.Textbox.prototype.getTotalWidth = function()
 {
-    return (this.width + this.marginLeft + this.marginRight);    
+    return (this.width + this.marginLeft + this.marginRight);
 };
 
 L.objects.Textbox.prototype.getTotalHeight = function()
 {
-    return (this.height + this.marginTop + this.marginBottom);    
+    return (this.height + this.marginTop + this.marginBottom);
 };
 
 L.objects.Textbox.prototype.realign = function()
 {
-  this[this.alignment]();
-  this[this.alignmentY]();
+    this[this.alignment]();
+    this[this.alignmentY]();
 };
 
 L.objects.Textbox.prototype.center = function()
 {
-  this.handle.x = (this.getTotalWidth()/2);
-  this.alignment = "center";
-  return this;
+    this.handle.x = (this.getTotalWidth() / 2);
+    this.alignment = "center";
+    return this;
 };
 
 L.objects.Textbox.prototype.centerY = function()
 {
-  this.handle.y = (this.getTotalHeight()/2);
-  this.alignmentY = "centerY";
-  return this;
+    this.handle.y = (this.getTotalHeight() / 2);
+    this.alignmentY = "centerY";
+    return this;
 };
 
 L.objects.Textbox.prototype.top = function()
 {
-  this.handle.y = 0;
-   this.alignmentY = "top";
-  return this;
+    this.handle.y = 0;
+    this.alignmentY = "top";
+    return this;
 };
 
 L.objects.Textbox.prototype.bottom = function()
 {
-  this.handle.y = this.getTotalHeight();
-   this.alignmentY = "bottom";
-  return this;
+    this.handle.y = this.getTotalHeight();
+    this.alignmentY = "bottom";
+    return this;
 };
 
 L.objects.Textbox.prototype.left = function()
 {
-  this.handle.x = 0;
-  this.alignment = "left";
-  return this;
+    this.handle.x = 0;
+    this.alignment = "left";
+    return this;
 };
 
 L.objects.Textbox.prototype.right = function()
 {
-  this.handle.x = this.getTotalWidth();
-  this.alignment = "right"; 
-  return this;
+    this.handle.x = this.getTotalWidth();
+    this.alignment = "right";
+    return this;
 };
 
 L.objects.Textbox.prototype.setMargins = function()
@@ -227,19 +289,19 @@ L.objects.Textbox.prototype.isClicked = function(mouseX, mouseY)
 {
     if (this.isClickable)
     {
-	if ((  this.angle === 0 &&
-		mouseX >= this.x  - this.handle.x &&
-		mouseX <= this.x + this.width + this.marginLeft + this.marginRight - this.handle.x &&
-		mouseY >= this.y  - this.handle.y &&
-		mouseY <= this.y + this.height + this.marginTop + this.marginBottom - this.handle.y
-		) || (
-		this.angle !== 0 &&
-		this.jordanCurve(mouseX,mouseY)))
+	if ((this.angle === 0 &&
+	mouseX >= this.x - this.handle.x &&
+	mouseX <= this.x + this.width + this.marginLeft + this.marginRight - this.handle.x &&
+	mouseY >= this.y - this.handle.y &&
+	mouseY <= this.y + this.height + this.marginTop + this.marginBottom - this.handle.y + (this.fontSize * (this.textArray.length - 1) * this.lineSpacing)
+	) || (
+	this.angle !== 0 &&
+	this.jordanCurve(mouseX, mouseY)))
 	{
-	  this.onClick();
+	    this.onClick();
 
 	    return true;
-	    
+
 	}
     }
 };
@@ -251,13 +313,13 @@ L.objects.Textbox.prototype.getVertices = function()
     var top = 0 - this.handle.y;
     var left = 0 - this.handle.x;
     var right = left + this.width + this.marginLeft + this.marginRight;
-    var bottom = top + this.height + this.marginTop + this.marginBottom;
-    var vertices = [[left,top],[right,top],[right,bottom],[left,bottom]];
-    if (this.angle !==0)
+    var bottom = top + this.height + this.marginTop + this.marginBottom + (this.fontSize * (this.textArray.length - 1) * this.lineSpacing);
+    var vertices = [[left, top], [right, top], [right, bottom], [left, bottom]];
+    if (this.angle !== 0)
     {
 	var length = vertices.length;
-	
-	for (var i =0; i < length; i++)
+
+	for (var i = 0; i < length; i++)
 	{
 	    vertices[i] = [
 		vertices[i][0] * Math.cos(-this.angle) - vertices[i][1] * Math.sin(-this.angle),
@@ -265,35 +327,35 @@ L.objects.Textbox.prototype.getVertices = function()
 	    ];
 	}
     }
-    
-    
+
+
     vertices.mapQuick(function(entry) {
 	entry[0] += xTransform;
 	entry[1] += yTransform;
     });
-    
-    
+
+
     return vertices;
-    
+
 };
 
 L.objects.Textbox.prototype.jordanCurve = function(x, y)
 {
-    
+
     var isInPoly = false;
     var vertices = this.getVertices();
     var length = vertices.length;
     //alert(vertices);
-    for (var i = 0, j = length-1; i< length; j= i++)
+    for (var i = 0, j = length - 1; i < length; j = i++)
     {
 	if ((vertices[i][1] > y) !== (vertices[j][1] > y))
 	{
-	    if (x < ((vertices[j][0] - vertices[i][0]) * (y - vertices[i][1])/(vertices[j][1]-vertices[i][1]) + vertices[i][0]))
+	    if (x < ((vertices[j][0] - vertices[i][0]) * (y - vertices[i][1]) / (vertices[j][1] - vertices[i][1]) + vertices[i][0]))
 	    {
 		isInPoly = !isInPoly;
 	    }
-	    
+
 	}
     }
-    return isInPoly; 
+    return isInPoly;
 };
