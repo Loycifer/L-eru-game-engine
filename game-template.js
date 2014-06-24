3cc27c631de6efebfb7b52c4d7260f8a2fa69b7e
Loy Clements <loy.clements@gmail.com>
2014-06-24 19:03:36 +0200

    Template scoping update
    Added support for L.globals namespace

--- game-template.js
+++ game-template.js
@@ -1,76 +1,74 @@
 var L;    //Do not remove this line or overwrite variable L
-function L_Game()
+
+L.game.globals = function() {
+//This is where you should Declare Variables
+//Since JavaScript is loosely typed, you may want to set variables with empty types
+//eg. this.score = 0; this.flags = []; this.name = "";
+
+
+//Scenes
+//eg. this.mainScene, secondScene;
+
+//Constants and Variables
+//eg. this.WIDTH = 640;
+
+//Objects and Arrays
+
+
+
+
+};
+
+L.game.settings = function() {
+
+
+    //This is where to adjust engine settings
+    L.system.width = 900;
+    L.system.height = 600;
+    //L.system.canvasLocation = document.getElementById("YOURDIV");
+    L.system.fullscreen = true;
+    L.system.orientation = "landscape";
+
+
+};
+
+
+L.game.resources = function() {
+    //This is where you load resources such as textures and audio
+    //Textures are stored in L.texture[x], where x is the texture's name
+    //Souns and music are similarly stored in L.sound[x] and
+    //L.music[x]
+
+    //eg. L.loadTexture.fromFile("littleDude", "little-dude.png");
+
+};
+
+
+L.game.initialise = function() {
+    //This is where to build game logic such as scenes, sprites,
+    //behaviours, and input handling
+    //Scenes are stores in L.scene[x], where x is the name of the scene
+
+    //eg. mainScene = new L.objects.Scene("mainScene");
+    //    mainScene.addLayer();
+    //    mainScene.layers[0].addObject(someExistingObject);
+    //    L.system.currentScene = mainScene;
+
+};
+
+
+L.game.update = function(dt)
 {
+    //This function can be left alone; it invokes every active object's update() automatically
+    L.system.currentScene.update(dt);
 
-    //This is where you should Declare Variables
-    //Since JavaScript is loosely typed, you may want to set variables with empty types
-    //eg. var score = 0; var flags = []; var name = "";
-
-
-    //Scenes
-    //eg. var mainScene, secondScene;
-
-    //Constants and Variables
-    //eg. var WIDTH = 640;
-
-    //Objects and Arrays
+};
 
 
-    //Input Handling
-    //eg. window.addEventListener("keydown", yourKeydownFunction, true);
+L.game.draw = function()
+{
+    //This function can be left alone; it invokes every active object's draw() automatically
+    L.system.currentScene.draw();
 
+};
 
-
-    this.settings = function() {
-
-
-	//This is where to adjust engine settings
-	L.system.width = 900;
-	L.system.height = 600;
-	//L.system.canvasLocation = document.getElementById("YOURDIV");
-	L.system.fullscreen = true;
-	L.system.orientation = "landscape";
-
-
-    };
-
-
-    this.resources = function() {
-	//This is where you load resources such as textures and audio
-	//Textures are stored in L.texture[x], where x is the texture's name
-	//Souns and music are similarly stored in L.sound[x] and
-	//L.music[x]
-
-	//eg. L.loadTexture.fromFile("littleDude", "little-dude.png");
-
-    };
-
-
-    this.initialise = function() {
-	//This is where to build game logic such as scenes, sprites,
-	//behaviours, and input handling
-	//Scenes are stores in L.scene[x], where x is the name of the scene
-
-	//eg. mainScene = new L.objects.Scene("mainScene");
-	//    mainScene.addLayer();
-	//    mainScene.layers[0].addObject(someExistingObject);
-	//    L.system.currentScene = mainScene;
-
-    };
-
-
-    this.update = function(dt)
-    {
-	//This function can be left alone; it invokes every active object's update() automatically
-	L.system.currentScene.update(dt);
-
-    };
-
-
-    this.draw = function()
-    {
-	//This function can be left alone; it invokes every active object's draw() automatically
-	L.system.currentScene.draw();
-
-    };
-}
