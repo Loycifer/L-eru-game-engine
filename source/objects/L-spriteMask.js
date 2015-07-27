6d05f27ae9da92a512ead042402f05dae1b62792
Loy Clements <loy.clements@gmail.com>
2015-07-27 18:47:11 +0200

    SpriteMask edit
    SpriteMask now keeps track of color and imageData

--- source/objects/L-spriteMask.js
+++ source/objects/L-spriteMask.js
@@ -4,6 +4,9 @@
 L.objects.SpriteMask = function (textureName,red,green,blue,alpha,width,height)
 {
 
+    this.red = red;
+    this.green = green;
+    this.blue = blue;
     var texture = this.sourceTexture = L.textures[textureName];
     var targetWidth = this.width = (width === undefined)?texture.width:width;
     var targetHeight = this.height = (height === undefined)?texture.height:height;
@@ -18,6 +21,7 @@ L.objects.SpriteMask = function (textureName,red,green,blue,alpha,width,height)
     maskCanvas.height = targetHeight;
     var ctx = this.ctx = maskCanvas.getContext("2d");
     ctx.drawImage(texture,0,0);
+      this.imageData = this.ctx.getImageData(0,0,targetWidth,targetHeight);
     this.setColor(red,green,blue,alpha);
 
 
@@ -32,7 +36,10 @@ L.objects.SpriteMask.prototype.setColor = function(red,green,blue,alpha)
 {
 var width = this.width;
 var height = this.height;
-    var imageData = this.ctx.getImageData(0,0,width,height);
+this.red = red;
+this.green = green;
+this.blue = blue;
+    var imageData = this.imageData;//this.ctx.getImageData(0,0,width,height);
     var dataLength = imageData.data.length;
     for (var i = 0; i < dataLength; i+=4)
     {
